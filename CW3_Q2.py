d963fa05df09701bfedbc39b93ab92f0e6046645
benjefferson457 <benjojefferson@gmail.com>
2026-03-19 11:08:19 +0000

    Update CW3_Q2.py

--- CW3_Q2.py
+++ CW3_Q2.py
@@ -70,3 +70,56 @@ plt.subplots_adjust(right = 0.75)
 plt.grid()
 plt.savefig('time_series_plots_(T,Y).png')
 plt.show()
+
+
+
+# Define time-stepping function
+def forward_euler(x0, y0, I):
+    
+    t = np.linspace(0, T, N)
+    
+    x = np.zeros(N)
+    y = np.zeros(N)
+    x[0] = x0
+    y[0] = y0
+
+    for n in range(N-1):
+        x[n+1] = x[n] + dt*(x[n] - 1/3*x[n]**3 - y[n] + I)
+        y[n+1] = y[n] + dt*((1/mu)*(x[n] - (a*y[n]) + b))
+
+    return x, y, t
+    
+# Choose 5 values of I between 0 and 2
+I_values = [0.0, 0.5, 1.0, 1.5, 2]
+
+plt.figure(figsize=(12,8))
+
+# Use for loop to make a subplot with graphs for all 5 values of I
+i = 0
+for I in I_values:
+    x, y, t = forward_euler(0, 0, I)
+
+    x_vals = np.linspace(-2, 2, 400)
+    x_null = x_vals - (1/3)*x_vals**3 + I
+    y_null = (x_vals + b)/a
+
+    plt.subplot(2, 3, i+1)
+
+    plt.plot(x_vals, x_null, linestyle='dashed', label='x-nullcline')
+    plt.plot(x_vals, y_null, linestyle='dashed', label='y-nullcline')
+    plt.plot(x, y, label='trajectory')
+
+    plt.title(f"I = {I}")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid()
+    plt.legend()
+    
+# Update i for next iteration
+    i += 1  
+
+# Complete plot
+plt.suptitle("Phase Plane for different values of I")
+plt.tight_layout()
+plt.savefig("Task2PhasePlanes.png")
+plt.show()
